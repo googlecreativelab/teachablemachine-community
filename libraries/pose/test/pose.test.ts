696979bdd17dc9be589152042f134d9e47529978
Irene Alvarado <ire.alvarado@gmail.com>
2019-11-04 20:37:25 -0500

    update pose tests because of breaking API change on webcam

--- libraries/pose/test/pose.test.ts
+++ libraries/pose/test/pose.test.ts
@@ -132,7 +132,7 @@ async function testPosenet(
 describe('Test pose library', () => {
     it('constants are set correctly', () => {
         assert.equal(typeof tm, 'object', 'tm should be an object');
-        assert.equal(typeof tm.getWebcam, 'function', 'tm.getWebcam should be a function');
+        assert.equal(typeof tm.Webcam, 'function', 'tm.Webcam should be a function');
         assert.equal(typeof tm.version, 'string', 'tm.version should be a string');
         assert.equal(tm.version, require('../package.json').version, "version does not match package.json.");
     });
